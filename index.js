5592390eecc1f35c0e3b881edd18ad50473dee15
Denis <rdenis24@yahoo.com>
2022-03-06 11:54:31 +0200

    main

---
+++ index.js
@@ -0,0 +1,15 @@
+function createStar() {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = Math.random() * 100 + 'vw';
+    star.style.animationDuration = Math.random() *2 +3 + 's';
+    star.innerText = '⭐'
+
+    document.body.appendChild(star);
+    setTimeout(()=>{
+        star.remove()
+    },3000)
+
+}
+
+setInterval(createStar, 300)
